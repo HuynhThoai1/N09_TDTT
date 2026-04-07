d3dbde272514c68f8cde2d1d04a9a84cf242548b
Do_Trung_Kien <24120350@student.hcmus.edu.vn>
2026-04-07 21:51:43 +0700

    Hoàn thành BE1-01 BE1-02 BE1-03

--- backend/tours/models.py
+++ backend/tours/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+class Node(models.Model):
+    name = models.CharField(max_length=100)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+
+class Edge(models.Model):
+    from_node = models.ForeignKey(
+        Node,
+        related_name="edges_from",
+        on_delete=models.CASCADE
+    )
+
+    to_node = models.ForeignKey(
+        Node,
+        related_name="edges_to",
+        on_delete=models.CASCADE
+    )
+
+    distance = models.FloatField()
+
+    def __str__(self):
+        return f"{self.from_node} -> {self.to_node}"
+
+
+class POI(models.Model):
+    name = models.CharField(max_length=100)
+
+    node = models.ForeignKey(
+        Node,
+        related_name="pois",
+        on_delete=models.CASCADE
+    )
+
+    description = models.TextField(blank=True)
+    category = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
