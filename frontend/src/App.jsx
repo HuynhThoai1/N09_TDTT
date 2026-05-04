9ded4b8202b0de48a3f5608b73cbc9f67fc341e9
Do_Trung_Kien <24120350@student.hcmus.edu.vn>
2026-05-04 18:06:56 +0700

    Hoàn thành hệ thống cá nhân hóa

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MainPage from "./pages/MainPage";
+import OnboardingPage from "./pages/OnboardingPage";
+
 const router = createBrowserRouter([
 	//Sau này có thêm các trang khác nếu muốn
 	{
@@ -9,6 +11,10 @@ const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <MainPage />,
+	},
+	{
+		path: "/onboarding",
+		element: <OnboardingPage />,
 	},
 ]);
 
