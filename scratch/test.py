49da426482f2e0054b616f5552465d5cd766e00a
HuynhThoai1 <24120457@student.hcmus.edu.vn>
2026-05-16 23:11:21 +0700

    Hoàn thành code frontend/backend đồ án (without testing)

--- scratch/test.py
+++
@@ -1,6 +0,0 @@
-import urllib.request
-try:
-    with urllib.request.urlopen("http://127.0.0.1:8000/api/goong/autocomplete/?input=nha") as response:
-        print(response.read().decode('utf-8'))
-except Exception as e:
-    print("Error:", e)
